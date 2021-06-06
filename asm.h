cfd2951a99e9957a578d42fa2f3754407fba301f
Sam M W <you@example.com>
2021-06-06 14:28:52 +0100

    Move doxygen stuff to header where it belongs

--- asm.h
+++ asm.h
@@ -1,4 +1,14 @@
 #include "decl.h"
 #include "stoc.h"
 
+/**
+ * @file
+ * @brief Parser for .stoc files
+ */
+
+/** Load a .stoc file
+ *
+ * @param filename The name of the file to load
+ * @param reference A pointer to the context_t to load the file into
+ */
 void readfile(char *filename, context_t *reference);
