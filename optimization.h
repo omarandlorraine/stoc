468f77a13d19618f53b75253cab71f73b787285e
Sam M W <you@example.com>
2021-06-06 14:54:42 +0100

    documentation for optimization.h

--- optimization.h
+++ optimization.h
@@ -1,8 +1,18 @@
 #include "stoc.h"
 
+/**
+ * @file
+ * @brief Functions for measuring rewrites.
+ */
+
+/// Returns an integer representing the size of the rewrite
 int optimize_size(context_t *c);
+
+/// Returns an integer representing the speed of the rewrite
 int optimize_speed(context_t *c);
 
-int compare(context_t *a, context_t *b);
-
+/// Sets the optimization function
 void set_optimization(int (*fn)(context_t *c));
+
+/// Returns an integer representing a comparison of two rewrites, according to whichever function was selected by set_optimization
+int compare(context_t *a, context_t *b);
