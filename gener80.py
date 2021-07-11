b06aa8d4197aaaf327b04a7080b67be4af034b4f
Sam M W <you@example.com>
2021-07-11 15:16:30 +0100

    add a few more opcodes

--- gener80.py
+++ gener80.py
@@ -20,8 +20,23 @@ def addopc(prefix, codepoint, group, dis1, dis2, length):
 	assert codepoint in range(256)
 	opcodes.append(Opcode(prefix, codepoint, group, dis1, dis2, length))
 
-# First add all the base opcodes; these are supported by the Intel 8080 and all others.
+def codepoint(prefix=0, x=0, y=0, z=0, p=0, q=0):
+	assert x in range(4)
+	assert p in range(4)
+	assert q in [0, 1]
+	assert y in range(8)
+	assert z in range(8)
+	if y != 0:
+		assert p == 0
+		assert q == 0
+	if p != 0 or q != 0:
+		assert y == 0
+	
+	return z + y * 8 + q * 8 + p * 16 + x * 64
 
+
+
+# First add all the base opcodes; these are supported by the Intel 8080 and all others.
 addopc("none", 0x00, "implied", "nop", "", 0)
 addopc("none", 0x27, "implied", "daa", "", 0)
 addopc("none", 0x07, "implied", "rlca", "", 0)
@@ -30,7 +45,22 @@ addopc("none", 0x0f, "implied", "rrca", "", 0)
 addopc("none", 0x1f, "implied", "rra", "", 0)
 
 for a,r in enumerate(rp):
-	addopc("none", 1 + a * 8, "rpimm16", "ld %s, " % rp[a], "", 2)
+	p = a << 4
+	addopc("none", 1 + p, "rpimm16", "ld %s, " % rp[a], "", 2)
+	addopc("none", 9 + p, "rpimm16", "add hl, %s" % rp[a], "", 0)
+
+# Next add the LR35902 specific opcodes
+if sys.argv[1] == "lr35902":
+	addopc("none", codepoint(y=1), "rpimm16", "ld (", "), sp", 0)
+	addopc("none", codepoint(y=2), "rpimm16", "stop", "", 0)
+
+
+# Next add the Z80 specific opcodes
+if sys.argv[1] == "z80":
+	addopc("none", codepoint(y=1), "implied", "ex af, af'", "", 0)
+# TODO: DJNZ
+
+
 
 print("#include \"stoc.h\"")
 print("#include \"arch-%s.h\"" % sys.argv[1])
@@ -49,10 +79,12 @@ for p in prefixes:
 	print("instrdata_t instrdata_%s[256] = {" % p)
 	for c in range(256):
 		o = [ o for o in opcodes if o.prefix == p and o.codepoint == c]
-		if len(o):
+		if len(o) == 1:
 			oc = o[0]
-			print("\t{disasm_%s, altop_%s, rndoper_%s, &instructions_%s, \"%s\", \"%s\", %d}," %
-				(oc.group, oc.group, oc.group, oc.group, oc.dis1, oc.dis2, oc.length))
+			print("\t/* 0x%02x */ {disasm_%s, altop_%s, rndoper_%s, &instructions_%s, \"%s\", \"%s\", %d}," %
+				(oc.codepoint, oc.group, oc.group, oc.group, oc.group, oc.dis1, oc.dis2, oc.length))
+		elif len(o) > 1:
+			print("ERROR: Duplicate codepoint 0x%02x" % c, file=sys.stderr);
 		else:
-			print("\t{0},")
+			print("\t/* 0x%02x */ {0}," % c)
 	print("};\n\n")
